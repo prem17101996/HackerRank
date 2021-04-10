4d8e9441089e479386e373b18e6d3bfad6ecd7ea
Prem <premmishra17101996@gmail.com>
2021-04-10 23:12:26 +0530

    Added Regex Substitution program

---
+++ Regex Substitution.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+for _ in range(int(input())):
+    line = input()
+    
+    while ' && ' in line or ' || ' in line:
+        line = line.replace(" && ", " and ").replace(" || ", " or ")
+    
+    print(line)
