8c826a2bc5a5bcadb16d56608d9265d07ddf9e9d
Prem <premmishra17101996@gmail.com>
2021-04-11 10:58:57 +0530

    Added a Any or All program

---
+++ Any or All.py
@@ -0,0 +1,10 @@
+t=input()
+num=(input().split())
+num.sort() 
+if int(num[0])>=1:
+    for i in num:       
+        if (i)==i[::-1]:           
+            print("True")
+            break
+else:
+    print("False")
