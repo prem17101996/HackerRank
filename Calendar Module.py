b7fb31c9c5c79eb3c5a5532e1a8710005ba811ee
Prem <premmishra17101996@gmail.com>
2021-04-27 17:56:07 +0530

    Added program by using calendar module

---
+++ Calendar Module.py
@@ -0,0 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import calendar
+month,date,year=input().split() 
+day=calendar.weekday(int(year),int(month),int(date))
+
+if day==6:
+    print("SUNDAY")
+elif day==0:
+    print("MONDAY")
+elif day==1:
+    print("TUESDAY")
+elif day==2:
+    print("WEDNESDAY")
+elif day==3:
+    print("THURSDAY")
+elif day==4:
+    print("FRIDAY")
+elif day==5:
+    print("SATURDAY") 
