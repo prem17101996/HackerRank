598953f99154ba8a3c8124d2f5a5e963179dca63
Prem <premmishra17101996@gmail.com>
2021-06-20 18:19:07 +0530

    Added a program by using aloop and if condition

---
+++ Divisible Sum Pairs.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'divisibleSumPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+#
+
+def divisibleSumPairs(n, k, ar):
+    lst=[]
+    for i in range(n):
+        for j in range(len(ar)-1):
+            lst.append(int(ar[0])+int(ar[j+1]))
+        ar.remove(ar[0])
+    count=0
+    for i in lst:
+        if i%k==0:
+            count+=1
+    return (count)       
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
