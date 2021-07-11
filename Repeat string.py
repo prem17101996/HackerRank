fdcc809efd0e58b841fb9d234afbaf0475d34429
Prem <premmishra17101996@gmail.com>
2021-07-11 08:38:28 +0530

    Added to solve repeate string problem

---
+++ Repeat string.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    div=int(n/len(s))
+    
+    mod=n%len(s)
+    result=s.count("a")*int(div)
+    
+    for i in range(mod):
+        if s[i]=="a":
+            result+=1
+    return(result)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
