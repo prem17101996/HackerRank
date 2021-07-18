39eac6f62417da20f6e7b49c1ee3e9c3ae9fbcff
Prem <premmishra17101996@gmail.com>
2021-07-18 17:07:06 +0530

    Added a program using if condition

---
+++ Alternating Characters .py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    lst=list(s[0])
+    lst1=[]   
+    dele=0
+    for i in s:
+        if i==lst[-1]:
+            dele+=1
+        else:
+            lst.append(i)
+    return(dele-1)   
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
