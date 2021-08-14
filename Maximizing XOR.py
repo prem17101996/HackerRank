97a33050a204e2027b3ce0269682a6aaa2d1ab1d
Prem <premmishra17101996@gmail.com>
2021-08-14 15:32:05 +0530

    Added a program by using xor

---
+++ Maximizing XOR.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maximizingXor' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER l
+#  2. INTEGER r
+#
+
+def maximizingXor(l,r):
+    lst=[]
+    for i in range(l,r+1):
+        
+        for j in range(l,r+1):
+            lst.append(i^j)
+    return(max(lst))
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    l = int(input().strip())
+
+    r = int(input().strip())
+
+    result = maximizingXor(l, r)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
